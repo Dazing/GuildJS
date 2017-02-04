7ccdfbfb5e764b8c415768fc35a95a5be331b38a
Carl-Henrik Braw <callebraw@gmail.com>
2017-02-04 11:44:30 +0100

    router

--- router.js
+++ router.js
@@ -7,24 +7,24 @@ var title = 'example'
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
+	console.log('Time: ', Date.now());
+	next();
 });
 
 // define the home page route
 router.get('/', function(req, res) {
-  res.render('index', params("index"));
+	res.render('index', params("index"));
 });
 
 // define the home page route
 router.get('/login', function(req, res) {
-  res.render('login', params("login"));
+	res.render('login', params("login"));
 });
 
 
 function renderParams (path) {
 	switch (path) {
-		case "login": 
+		case "login":
 				{
 					pageTitle: title,
 					boolHeader: false,
