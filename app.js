0861b8ca6249e32dcc297e6b811f248a51f6338b
Carl-Henrik Braw <carl@chbraw.com>
2017-02-18 13:43:01 +0100

    forgot a few things

--- app.js
+++ app.js
@@ -26,11 +26,7 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: false, save
 
 app.use(passport.initialize());
 
-//app.use('/', router);
-
-app.get('/*',function(req,res){
-	res.sendFile(path.join(__dirname,'/my-app/src/index.html'));
-})
+app.use('/', router);
 
 // ----------------------------------------------------
 // Server functionality
