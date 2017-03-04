f19462f39786fca2702acb9f9ad949bc81d44af8
tobiasolausson <tobola@student.chalmers.com>
2017-03-04 11:19:55 +0100

    Authenticated users can now add sections

--- model/forum.js
+++ model/forum.js
@@ -25,6 +25,27 @@ forumSchema.methods.findById = function(id, callback) {
 	});
 };
 
+
+forumSchema.methods.addSection = function(name, description, callback) {
+
+		var newSection = new forum();
+
+		newSection.sectionid = mongoose.Types.ObjectId();
+		newSection.name = name;
+		newSection.description = description;
+
+		newSection.save(function(err, savedSection){
+			if (err) {
+				callback(err);
+			}else{
+				callback(null,savedSection);
+			}
+		});
+
+};
+
+
+
 forumSchema.methods.insertTestData = function() {
 	var i = 0;
 
